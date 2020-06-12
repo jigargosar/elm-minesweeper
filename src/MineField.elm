bcb680343afaabc117074f874176577db4e3af32
Jigar Gosar <jigar.gosar@gmail.com>
2020-06-12 14:55:41 +0530

    minesweeper

--- src/MineField.elm
+++ src/MineField.elm
@@ -30,7 +30,63 @@ generator size minePct =
 
 initCellDict : I2 -> Set I2 -> CellDict
 initCellDict size minePositions =
-    Debug.todo "impl"
+    let
+        neighbourMineCount pos =
+            neighbourOf pos
+                |> Set.foldl
+                    (\nPos count ->
+                        if Set.member nPos minePositions then
+                            count + 1
+
+                        else
+                            count
+                    )
+                    0
+    in
+    Set.foldl
+        (\pos ->
+            Dict.insert pos
+                (if Set.member pos minePositions then
+                    Mine
+
+                 else
+                    Empty (neighbourMineCount pos)
+                )
+        )
+        Dict.empty
+        (positionsFromSize size)
+
+
+validNeighboursInSize : I2 -> I2 -> Set I2
+validNeighboursInSize size pos =
+    neighbourOf pos
+        |> Set.filter (isValidInSize size)
+
+
+isValidInSize : I2 -> I2 -> Bool
+isValidInSize ( w, h ) ( x, y ) =
+    not (x < 0 || y < 0 || x >= w || y >= h)
+
+
+neighbourOf ( x, y ) =
+    Set.map (\( dx, dy ) -> ( x + dx, y + dy )) unitNeighbours
+
+
+unitNeighbours =
+    [ [ ( -1, -1 ), ( 0, -1 ), ( 1, -1 ) ]
+    , [ ( -1, 0 ), ( 1, 0 ) ]
+    , [ ( -1, 1 ), ( 0, 1 ), ( 1, 1 ) ]
+    ]
+        |> List.concat
+        |> Set.fromList
+
+
+positionsFromSize : I2 -> Set I2
+positionsFromSize ( w, h ) =
+    List.range 0 (w - 1)
+        |> List.map (\x -> List.range 0 (h - 1) |> List.map (\y -> ( x, y )))
+        |> List.concat
+        |> Set.fromList
 
 
 minePositionsGenerator : I2 -> Float -> Generator (Set I2)
