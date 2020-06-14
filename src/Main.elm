cb4ec600ed206eb2da8244e0d4a63334a8e6250a
Jigar Gosar <jigar.gosar@gmail.com>
2020-06-14 14:55:44 +0530

    minesweeper

--- src/Main.elm
+++ src/Main.elm
@@ -90,7 +90,7 @@ update msg model =
         ( PlayerTurn, Click pos ) ->
             let
                 _ =
-                    openPos pos model
+                    openLid pos model
             in
             case tileAt model pos of
                 Just ( LG.Closed, MG.Mine ) ->
@@ -123,8 +123,8 @@ update msg model =
             model
 
 
-openPos : ( Int, Int ) -> Model -> Maybe ( GameState, LidGrid )
-openPos pos model =
+openLid : ( Int, Int ) -> Model -> Maybe ( GameState, LidGrid )
+openLid pos model =
     case tileAt model pos of
         Just ( LG.Closed, MG.Mine ) ->
             Just
