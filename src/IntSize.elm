2022273ce12494e93e2f89d402033d68a0e28d8e
Jigar Gosar <jigar.gosar@gmail.com>
2020-06-14 19:23:49 +0530

    minesweeper

--- src/IntSize.elm
+++ src/IntSize.elm
@@ -21,39 +21,38 @@ init w h =
     Size ( w, h )
 
 
-isPosInvalid : IntSize -> ( Int, Int ) -> Bool
-isPosInvalid (Size ( w, h )) ( x, y ) =
-    x < 0 || y < 0 || x >= w || y >= h
-
-
-isPosValid : IntSize -> ( Int, Int ) -> Bool
-isPosValid s =
-    isPosInvalid s >> not
-
-
 foldPositions : (Int2 -> a) -> IntSize -> List a
 foldPositions f (Size ( w, h )) =
     List.range 0 (w - 1)
         |> List.concatMap (\x -> List.range 0 (h - 1) |> List.map (\y -> f ( x, y )))
 
 
-positions : IntSize -> List ( Int, Int )
+positions : IntSize -> List Int2
 positions =
     foldPositions identity
 
 
-neighbours : IntSize -> ( Int, Int ) -> List ( Int, Int )
+neighbours : IntSize -> Int2 -> List Int2
 neighbours size pos =
+    let
+        isPosInvalid : IntSize -> Int2 -> Bool
+        isPosInvalid (Size ( w, h )) ( x, y ) =
+            x < 0 || y < 0 || x >= w || y >= h
+
+        isPosValid : IntSize -> Int2 -> Bool
+        isPosValid s =
+            isPosInvalid s >> not
+    in
     Tuple.neighboursOf pos
         |> List.filter (isPosValid size)
 
 
-neighbourSet : IntSize -> ( Int, Int ) -> Set ( Int, Int )
+neighbourSet : IntSize -> Int2 -> Set Int2
 neighbourSet size =
     neighbours size >> Set.fromList
 
 
-includeNeighbours : IntSize -> Set ( Int, Int ) -> Set ( Int, Int )
+includeNeighbours : IntSize -> Set Int2 -> Set Int2
 includeNeighbours size posSet =
     posSet
         |> Set.foldl (neighbourSet size >> Set.union) posSet
