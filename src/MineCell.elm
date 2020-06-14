86fc938a4c28f8833979deac64580e38485ecdf5
Jigar Gosar <jigar.gosar@gmail.com>
2020-06-14 17:16:44 +0530

    minesweeper

--- src/MineCell.elm
+++ src/MineCell.elm
@@ -1,26 +1,15 @@
 module MineCell exposing
     ( MineCell(..)
     , autoOpenPosSetFrom
-    , generator
     )
 
 import Grid exposing (Grid)
-import IntSize as Size exposing (IntSize)
-import List.Extra as List
-import More.Tuple as Tuple
-import Random exposing (Generator)
 import Set exposing (Set)
 
 
 type MineCell
     = Mine
     | Empty Int
-
-
-generator : IntSize -> Float -> Generator (Grid MineCell)
-generator size minePct =
-    minePosSetGenerator size minePct
-        |> Random.map (initMineCellGrid size)
 
 
 autoOpenPosSetFrom : ( Int, Int ) -> Grid MineCell -> Set ( Int, Int )
@@ -64,38 +53,3 @@ neighboursHavingZeroSurroundingMines grid pos =
 
 isEmptyWithNoSurroundingMines dict pos =
     Grid.get pos dict == Just (Empty 0)
-
-
-initMineCellGrid : IntSize -> Set ( Int, Int ) -> Grid MineCell
-initMineCellGrid size minePosSet =
-    let
-        isMine pos =
-            Set.member pos minePosSet
-
-        neighbourMineCount pos =
-            Tuple.neighboursOf pos |> List.count isMine
-    in
-    Grid.init size
-        (\pos ->
-            if Set.member pos minePosSet then
-                Mine
-
-            else
-                Empty (neighbourMineCount pos)
-        )
-
-
-minePosSetGenerator : IntSize -> Float -> Generator (Set ( Int, Int ))
-minePosSetGenerator size minePct =
-    let
-        xs =
-            Size.positions size
-    in
-    Random.list (List.length xs) (Random.weighted ( minePct, True ) [ ( 1 - minePct, False ) ])
-        |> Random.map
-            (\boolList ->
-                List.map2 Tuple.pair xs boolList
-                    |> List.filter Tuple.second
-                    |> List.map Tuple.first
-                    |> Set.fromList
-            )
