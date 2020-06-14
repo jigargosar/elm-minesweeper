93547e88e6ccdf4b954ae7b4a7d1c40e77224d1e
Jigar Gosar <jigar.gosar@gmail.com>
2020-06-14 18:49:08 +0530

    minesweeper

--- src/Board.elm
+++ src/Board.elm
@@ -1,6 +1,5 @@
 module Board exposing (Board, State(..), cycleLabel, generator, openLidAt, toDict)
 
-import Dict exposing (Dict)
 import Grid exposing (Grid)
 import IntSize as Size exposing (IntSize)
 import Lid exposing (Lid)
@@ -12,13 +11,13 @@ import Set exposing (Set)
 
 
 type Board
-    = Board (Grid Lid) (Grid MineCell)
+    = Board (Grid ( Lid, MineCell ))
 
 
 generator : IntSize -> Generator Board
 generator size =
-    mineGridGenerator size 0.1
-        |> Random.map (Board (Grid.filled size Lid.Closed))
+    minePosSetGenerator size 0.1
+        |> Random.map (initMineCellGrid size >> Board)
 
 
 type State
@@ -27,8 +26,8 @@ type State
 
 
 openLidAt : ( Int, Int ) -> Board -> Maybe ( State, Board )
-openLidAt pos (Board lids mines) =
-    case computeLidPositionsToOpen pos lids mines of
+openLidAt pos (Board grid) =
+    case computeLidPositionsToOpen pos grid of
         Nothing ->
             Nothing
 
@@ -38,118 +37,113 @@ openLidAt pos (Board lids mines) =
                 , Board
                     (Set.foldl
                         lidOpenIfClosed
-                        lids
+                        grid
                         toOpen
                     )
-                    mines
                 )
 
 
-computeLidPositionsToOpen start lids mines =
-    if lidsCanOpenAt start lids then
-        case Grid.get start mines of
-            Nothing ->
-                Nothing
+computeLidPositionsToOpen start grid =
+    case Grid.get start grid of
+        Nothing ->
+            Nothing
 
-            Just mineCell ->
+        Just ( lid, mineCell ) ->
+            if lid == Lid.Closed then
                 case mineCell of
                     Mine.Mine ->
                         Just ( Lost, Set.singleton start )
 
                     Mine.Empty 0 ->
-                        Just ( PlayerTurn, computeAutoOpenLidPositions lids mines (Set.singleton start) Set.empty )
+                        Just ( PlayerTurn, computeAutoOpenLidPositions grid (Set.singleton start) Set.empty )
 
                     Mine.Empty _ ->
                         Just ( PlayerTurn, Set.singleton start )
 
-    else
-        Nothing
+            else
+                Nothing
 
 
-computeAutoOpenLidPositions : Grid Lid -> Grid MineCell -> Set ( Int, Int ) -> Set ( Int, Int ) -> Set ( Int, Int )
-computeAutoOpenLidPositions lids mines pending acc =
+computeAutoOpenLidPositions grid pending acc =
     case Set.toList pending of
         [] ->
-            Grid.includeNeighbours mines acc
-                |> Set.filter (\pos -> lidsCanOpenAt pos lids)
+            Grid.includeNeighbours grid acc
+                |> Set.filter (\pos -> canOpenLidAt pos grid)
 
         current :: rest ->
             let
                 toCompute =
-                    Grid.neighbourSet current mines
-                        |> Set.filter (\pos -> canAutoOpenLidAt pos lids mines && not (Set.member pos acc))
+                    Grid.neighbourSet current grid
+                        |> Set.filter (\pos -> canAutoOpenLidAt pos grid && not (Set.member pos acc))
 
                 nPending =
                     Set.union toCompute (Set.fromList rest)
             in
-            computeAutoOpenLidPositions lids mines nPending (Set.insert current acc)
+            computeAutoOpenLidPositions grid nPending (Set.insert current acc)
 
 
-canAutoOpenLidAt pos lids mines =
-    lidsCanOpenAt pos lids && Grid.get pos mines == Just (Mine.Empty 0)
+canAutoOpenLidAt pos grid =
+    case Grid.get pos grid of
+        Just ( Lid.Closed, Mine.Empty 0 ) ->
+            True
+
+        _ ->
+            False
 
 
-lidsCanOpenAt pos lids =
-    Grid.get pos lids == Just Lid.Closed
+canOpenLidAt pos grid =
+    case Grid.get pos grid of
+        Just ( Lid.Closed, _ ) ->
+            True
+
+        _ ->
+            False
 
 
-lidOpenIfClosed : ( Int, Int ) -> Grid Lid -> Grid Lid
 lidOpenIfClosed pos =
     Grid.update pos
-        (\lid ->
+        (\( lid, mine ) ->
             if lid == Lid.Closed then
-                Lid.Open
+                ( Lid.Open, mine )
 
             else
-                lid
+                ( lid, mine )
         )
 
 
 cycleLabel : ( Int, Int ) -> Board -> Maybe Board
-cycleLabel pos (Board l m) =
+cycleLabel pos (Board grid) =
     let
         nl =
             Grid.update pos
-                (\lid ->
+                (\( lid, mine ) ->
                     case lid of
                         Lid.Open ->
-                            lid
+                            ( lid, mine )
 
                         Lid.Closed ->
-                            Lid.Flagged
+                            ( Lid.Flagged, mine )
 
                         Lid.Flagged ->
-                            Lid.Closed
+                            ( Lid.Closed, mine )
                 )
-                l
+                grid
     in
-    if Grid.get pos l /= Just Lid.Open then
-        Board nl m
-            |> Just
+    case Grid.get pos grid of
+        Just ( Lid.Open, _ ) ->
+            Nothing
 
-    else
-        Nothing
+        _ ->
+            Just (Board nl)
 
 
 toDict : Board -> PosDict ( Lid, MineCell )
-toDict (Board l m) =
-    Dict.merge
-        (\_ _ -> identity)
-        (\k v1 v2 -> Dict.insert k ( v1, v2 ))
-        (\_ _ -> identity)
-        (Grid.toDict l)
-        (Grid.toDict m)
-        Dict.empty
+toDict (Board grid) =
+    Grid.toDict grid
 
 
 
 -- Mine Grid
-
-
-mineGridGenerator : IntSize -> Float -> Generator (Grid MineCell)
-mineGridGenerator size minePct =
-    minePosSetGenerator size minePct
-        |> Random.map (initMineCellGrid size)
 
 
 minePosSetGenerator : IntSize -> Float -> Generator (Set ( Int, Int ))
@@ -168,7 +162,6 @@ minePosSetGenerator size minePct =
             )
 
 
-initMineCellGrid : IntSize -> Set ( Int, Int ) -> Grid MineCell
 initMineCellGrid size minePosSet =
     let
         isMine pos =
@@ -180,8 +173,8 @@ initMineCellGrid size minePosSet =
     Grid.init size
         (\pos ->
             if Set.member pos minePosSet then
-                Mine.Mine
+                ( Lid.Closed, Mine.Mine )
 
             else
-                Mine.Empty (neighbourMineCount pos)
+                ( Lid.Closed, Mine.Empty (neighbourMineCount pos) )
         )
