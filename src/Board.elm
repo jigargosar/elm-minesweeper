230a6ed907cafbe1b59bc64066e7c9daa7ff9328
Jigar Gosar <jigar.gosar@gmail.com>
2020-06-14 17:11:15 +0530

    minesweeper

--- src/Board.elm
+++ src/Board.elm
@@ -4,19 +4,19 @@ import Dict exposing (Dict)
 import Grid exposing (Grid)
 import IntSize exposing (IntSize)
 import Lid exposing (Lid)
-import MineCell as Mines
+import MineCell as Mine exposing (MineCell)
 import PosDict exposing (PosDict)
 import Random exposing (Generator)
 import Set
 
 
 type Board
-    = Board IntSize (Grid Lid) (Grid Mines.MineCell)
+    = Board IntSize (Grid Lid) (Grid MineCell)
 
 
 generate : IntSize -> Generator Board
 generate size =
-    Mines.generator size 0.1
+    Mine.generator size 0.1
         |> Random.map (Board size (Grid.filled size Lid.Closed))
 
 
@@ -27,20 +27,20 @@ type State
 
 openLid : ( Int, Int ) -> Board -> Maybe ( State, Board )
 openLid pos (Board size lids mines) =
-    case dictGet2 pos (Grid.toDict lids) (Mines.toDict mines) of
-        Just ( Lid.Closed, Mines.Mine ) ->
+    case dictGet2 pos (Grid.toDict lids) (Mine.toDict mines) of
+        Just ( Lid.Closed, Mine.Mine ) ->
             Just
                 ( Lost
                 , Board size (Grid.set pos Lid.Open lids) mines
                 )
 
-        Just ( Lid.Closed, Mines.Empty _ ) ->
+        Just ( Lid.Closed, Mine.Empty _ ) ->
             let
                 nLids =
                     Set.foldl
                         lidOpenIfClosed
                         lids
-                        (Mines.autoOpenPosSetFrom pos mines)
+                        (Mine.autoOpenPosSetFrom pos mines)
             in
             Just
                 ( PlayerTurn
@@ -89,14 +89,14 @@ cycleLabel pos (Board s l m) =
         Nothing
 
 
-toDict : Board -> PosDict ( Lid, Mines.MineCell )
+toDict : Board -> PosDict ( Lid, MineCell )
 toDict (Board _ l m) =
     Dict.merge
         (\_ _ -> identity)
         (\k v1 v2 -> Dict.insert k ( v1, v2 ))
         (\_ _ -> identity)
         (Grid.toDict l)
-        (Mines.toDict m)
+        (Mine.toDict m)
         Dict.empty
 
 
